2c9f87a0b82ead0cfb7583789d25412115ac087a
rafistern1986 <rsternbuch@gmail.com>
2014-10-26 01:10:34 +0200

    Update books.js

--- books.js
+++ books.js
@@ -1,79 +1,105 @@
 var addBotton = document.getElementById("addTvShow");
+var x = 1;
+var y = 2;
 
-	addBotton.onclick = function(){
-		var topDiv = document.getElementById("firstDiv");
-		
+	addBotton.onclick = function(x,y){
+		var enteringDiv = document.getElementById("enteringDiv");
+		if(enteringDiv.innerHTML == ''){
+				
 		var enterMovieName = document.createElement("input");
-		enterMovieName.setAttribute("button", "text");
-		enterMovieName.setAttribute("placeholder", "Book name");
+		enterMovieName.setAttribute("type", "text");
+		enterMovieName.setAttribute("placeholder", "TV show name");
 		enterMovieName.setAttribute("id", "bookName");
 		
 		var enterAuthorName = document.createElement("input");
-		enterAuthorName.setAttribute("button", "text");
-		enterAuthorName.setAttribute("placeholder", "Author name");
+		enterAuthorName.setAttribute("type", "text");
+		enterAuthorName.setAttribute("placeholder", "What Season");
 		enterAuthorName.setAttribute("id", "authorName");
 		
+		var enterWhatEpisode = document.createElement("input");
+		enterWhatEpisode.setAttribute("type", "text");
+		enterWhatEpisode.setAttribute("placeholder", "What Episode");
+		enterWhatEpisode.setAttribute("id", "episode");
+		
 		var enterRating = document.createElement("input");
-		enterRating.setAttribute("button", "text");
+		enterRating.setAttribute("type", "text");
 		enterRating.setAttribute("placeholder", "Give a score(1-10)");
 		enterRating.setAttribute("id", "score");
 		
 		var addTheBookButton = document.createElement("button");
-		addTheBookButton.setAttribute("onclick", "addBook()");
+		addTheBookButton.setAttribute("onclick", "addBook(x)");
+		addTheBookButton.setAttribute("id", "button");
 		
-		var textForButoon = document.createTextNode("Add this TV show");
+		var addTheBookButton2 = document.createElement("button");
+		addTheBookButton2.setAttribute("onclick", "addBook(y)");
+		addTheBookButton2.setAttribute("id", "button2");
+		
+		var textForButoon = document.createTextNode("Add to watch list");
 		addTheBookButton.appendChild(textForButoon);
-		
-		var enteringDiv = document.createElement("div");
-		enteringDiv.setAttribute("id", "enteringDiv");
-		
+		var textForButoon = document.createTextNode("Add to favorites");
+		addTheBookButton2.appendChild(textForButoon);
+	
 		enteringDiv.appendChild(enterMovieName);
 		enteringDiv.appendChild(enterAuthorName);
+		enteringDiv.appendChild(enterWhatEpisode);
 		enteringDiv.appendChild(enterRating);
 		enteringDiv.appendChild(addTheBookButton);
+		enteringDiv.appendChild(addTheBookButton2);
 		
-		topDiv.appendChild(enteringDiv);
+		var topDiv = document.getElementById("firstDiv");
+		topDiv.appendChild(enteringDiv);}else{
+			reset();
+		}
 		
 	}
 
-function Book (bookName, authorName, score) {
+function Book (bookName, authorName, episode, score, k) {
 	this.bookName = bookName;
 	this.authorName = authorName;
+	this.episode = episode;
 	this.score = score;
+	this.k = k;
 };
 
 function reset(){
-	document.getElementById("enteringDiv").remove();
-
+	document.getElementById("bookName").remove();
+document.getElementById("authorName").remove();
+document.getElementById("episode").remove();
+document.getElementById("score").remove();
+document.getElementById("button").remove();
+document.getElementById("button2").remove();
 }
 
-function addBook(){
+function addBook(k){
 	var bookName = document.getElementById('bookName').value;
 	var authorName = document.getElementById('authorName').value;
+	var episode = document.getElementById('episode').value;
 	var score = document.getElementById('score').value;
+	var k = k;
+	
 	
 	var lengthOfBookName = bookName.length;
 	var lengthOfAuther = authorName.length;
+	var lengthOfEpisode = episode.length;
 	var lengthOfScore = score.length;
 	
-	if (lengthOfBookName > 0 && lengthOfAuther > 0 && lengthOfScore > 0){
+	if (lengthOfBookName > 0 && lengthOfAuther > 0 && lengthOfEpisode > 0 && lengthOfScore > 0){
 		if(score > 0 && score < 11){
-		var book = new Book(bookName, authorName, score);
-		addToList(book);
-		reset();
-		}else{
+				var book = new Book(bookName, authorName, episode, score, k);
+				reset();
+				addToList(book);
+			}
+		else{
 			alert("please enter a number from 1-10")
 		}
 	}else{
 		alert("please enter all fields");
-	}
-	
-	
+	}	
 }
-
 function addToList(book) {
+
+		
 		var newElement = document.createElement("li");
-		newElement.id = "liList"
 		
 		var bookNameDiv = document.createElement("div");
 		bookNameDiv.innerHTML = book.bookName;
@@ -83,6 +109,10 @@ function addToList(book) {
 		authorNameDiv.innerHTML = book.authorName;
 		authorNameDiv.className = "center";
 		
+		var episodeDiv = document.createElement("div");
+		episodeDiv.innerHTML = book.episode;
+		episodeDiv.className = "center";
+		
 		var scoreDiv = document.createElement("div");
 		scoreDiv.innerHTML = book.score;
 		scoreDiv.className = "right";
@@ -90,16 +120,29 @@ function addToList(book) {
 		var garbege = document.createElement("button");
 		garbege.className = "garbage";
 		garbege.onclick = function whenClickOnGarbege(){
-							
-							var parentLi = garbege.parentNode;
-							parentLi.id = "parentLiOfGarbege"
-							document.getElementById("parentLiOfGarbege").remove();			
+								newElement.remove();		
 							};
 		
 		newElement.appendChild(bookNameDiv);
 		newElement.appendChild(authorNameDiv);
+		newElement.appendChild(episodeDiv);
 		newElement.appendChild(scoreDiv);
 		newElement.appendChild(garbege);
-		var ul = document.getElementById("bookList");
+		
+		if (book.k == 1){
+			var deleteMe = document.getElementById('warningEmptyWatchList');
+			if(deleteMe !== null){
+				deleteMe.remove();
+			}	
+			var ul = document.getElementById("bookList");
+			//ul.appendChild(newElement);
+		}else{
+			var deleteMe = document.getElementById('warningEmptyFavoritesList');
+			if(deleteMe !== null){
+				deleteMe.remove();
+		}	
+			var ul = document.getElementById("bookList2");
+			//ul.appendChild(newElement);
+		}
 		ul.appendChild(newElement);
 }
